b17a032dda5822ed2ad2241c6acbd238cd32f053
Vladimir Diaconescu <vladimir.diaconescu@cs.pub.ro>
2017-03-01 17:01:35 +0200

    [tema1][util] Added Makefiles for generating hash.dll and libhash.so

--- 1-multi/hash.h
+++ 1-multi/hash.h
@@ -2,7 +2,20 @@
 #ifndef HASH_H_
 #define HASH_H_
 
+#ifdef WIN
+#ifdef DLL_IMPORTS
+#define DLL_DECLSPEC __declspec(dllimport)
+#else
+#define DLL_DECLSPEC __declspec(dllexport)
+#endif
+
+DLL_DECLSPEC unsigned int hash(const char *str, unsigned int hash_length);
+
+#else
+
 unsigned int hash(const char *str, unsigned int hash_length);
+
+#endif
 
 #endif
 
