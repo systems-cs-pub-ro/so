f50673ebfd037d1c1987fe99b40e261ec87306c7
Razvan Crainea <razvan@opensips.org>
2018-04-12 16:10:54 +0300

    [tema3] fix code syntax for windows

--- 3-loader/skel-win/loader/debug.h
+++ 3-loader/skel-win/loader/debug.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 
 #if defined DEBUG
-#define dprintf(format, ...)                                    \
-	fprintf(stderr, " [%s(), %s:%u] " format,               \
-		__FUNCTION__, __FILE__, __LINE__,               \
+#define dprintf(format, ...) \
+	fprintf(stderr, " [%s(), %s:%u] " format, \
+		__FUNCTION__, __FILE__, __LINE__, \
 		##__VA_ARGS__)
 #else
-#define dprintf(format, ...)                                    \
+#define dprintf(format, ...) \
 	do {} while (0)
 #endif
 
--- 3-loader/skel-win/loader/exec_parser.c
+++ 3-loader/skel-win/loader/exec_parser.c
@@ -10,14 +10,15 @@
 
 #include "exec_parser.h"
 
-static BOOL SolveImports(uintptr_t loadBase, IMAGE_NT_HEADERS *peHdr);
+static BOOL SolveImports(uintptr_t loadBase, IMAGE_NT_HEADERS * peHdr);
 
 static int readHeader(HANDLE hFile, DWORD offset, char *buf, DWORD size)
 {
 	DWORD bytesRead, totalBytesRead;
 	BOOL bRet;
 
-	if (SetFilePointer(hFile, offset, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER) {
+	if (SetFilePointer(hFile, offset, NULL, FILE_BEGIN) ==
+			INVALID_SET_FILE_POINTER) {
 		fprintf(stderr, "Couldn't change offset: %d\n", GetLastError());
 		goto error;
 	}
@@ -50,14 +51,17 @@ so_exec_t *so_parse_exec(char *path)
 	so_seg_t *seg;
 	HANDLE hFile;
 
-	hFile = CreateFile(path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING,
+	hFile = CreateFile(path, GENERIC_READ, FILE_SHARE_READ,
+			NULL, OPEN_EXISTING,
 		FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hFile == INVALID_HANDLE_VALUE) {
-		fprintf(stderr, "Couldn't open file %s: %d\n", path, GetLastError());
+		fprintf(stderr, "Couldn't open file %s: %d\n",
+				path, GetLastError());
 		return NULL;
 	}
 
-	if (readHeader(hFile, 0, (char *)&dosHdr, sizeof(IMAGE_DOS_HEADER)) < 0) {
+	if (readHeader(hFile, 0, (char *)&dosHdr,
+			sizeof(IMAGE_DOS_HEADER)) < 0) {
 		fprintf(stderr, "Couldn't read PE DOS header\n");
 		goto out;
 	}
@@ -67,7 +71,8 @@ so_exec_t *so_parse_exec(char *path)
 		goto out;
 	}
 
-	if (readHeader(hFile, dosHdr.e_lfanew, (char *)&peHdr, sizeof(IMAGE_NT_HEADERS)) < 0) {
+	if (readHeader(hFile, dosHdr.e_lfanew,
+			(char *)&peHdr, sizeof(IMAGE_NT_HEADERS)) < 0) {
 		fprintf(stderr, "Couldn't read PE DOS header\n");
 		goto out;
 	}
@@ -85,7 +90,8 @@ so_exec_t *so_parse_exec(char *path)
 	}
 
 	if (readHeader(hFile, dosHdr.e_lfanew + sizeof(IMAGE_NT_HEADERS),
-			(char *)secHdr, nr_sections * sizeof(IMAGE_SECTION_HEADER)) < 0) {
+			(char *)secHdr, nr_sections *
+			sizeof(IMAGE_SECTION_HEADER)) < 0) {
 		fprintf(stderr, "Couldn't read PE DOS header\n");
 		goto out_free;
 	}
@@ -97,16 +103,20 @@ so_exec_t *so_parse_exec(char *path)
 	}
 
 	exec->base_addr = peHdr.OptionalHeader.ImageBase;
-	exec->entry = peHdr.OptionalHeader.ImageBase + peHdr.OptionalHeader.AddressOfEntryPoint;
-	exec->segments_no = peHdr.FileHeader.NumberOfSections + 1 /* an extra segment for headers */;
-	exec->segments = (so_seg_t *)malloc(exec->segments_no * sizeof(so_seg_t));
+	exec->entry = peHdr.OptionalHeader.ImageBase +
+		peHdr.OptionalHeader.AddressOfEntryPoint;
+	exec->segments_no = peHdr.FileHeader.NumberOfSections +
+		1 /* an extra segment for headers */;
+	exec->segments =
+		(so_seg_t *)malloc(exec->segments_no * sizeof(so_seg_t));
 
 	i =  0;
 	for (i = 0; i < peHdr.FileHeader.NumberOfSections; i++) {
 		IMAGE_SECTION_HEADER *sec = &secHdr[i];
-		seg = &exec->segments[i];
 
-		seg->vaddr = peHdr.OptionalHeader.ImageBase + sec->VirtualAddress;
+		seg = &exec->segments[i];
+		seg->vaddr = peHdr.OptionalHeader.ImageBase +
+			sec->VirtualAddress;
 		seg->offset = sec->PointerToRawData;
 		seg->file_size = sec->SizeOfRawData;
 		seg->mem_size = sec->Misc.VirtualSize;
@@ -141,14 +151,16 @@ out:
 void so_start_exec(so_exec_t *exec, char *argv[])
 {
 	IMAGE_NT_HEADERS *peHdr = (IMAGE_NT_HEADERS *)
-		(((IMAGE_DOS_HEADER *)(exec->base_addr))->e_lfanew + (DWORD)(exec->base_addr));
+		(((IMAGE_DOS_HEADER *)(exec->base_addr))->e_lfanew +
+		 (DWORD)(exec->base_addr));
 	SolveImports(exec->base_addr, peHdr);
 	((void(*)(void))exec->entry)();
 }
 
 static BOOL SolveImports(uintptr_t loadBase, IMAGE_NT_HEADERS *peHdr)
 {
-	IMAGE_DATA_DIRECTORY *dir = &peHdr->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+	IMAGE_DATA_DIRECTORY *dir =
+	&peHdr->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
 	DWORD *oft;
 	DWORD *ft;
 	IMAGE_IMPORT_BY_NAME *impName;
@@ -167,7 +179,8 @@ static BOOL SolveImports(uintptr_t loadBase, IMAGE_NT_HEADERS *peHdr)
 
 		hModule = LoadLibrary(dllName);
 		if (!hModule) {
-			fprintf(stderr, "Failed to load %s: %d\n", dllName, GetLastError());
+			fprintf(stderr, "Failed to load %s: %d\n",
+					dllName, GetLastError());
 			return FALSE;
 		}
 
@@ -177,18 +190,25 @@ static BOOL SolveImports(uintptr_t loadBase, IMAGE_NT_HEADERS *peHdr)
 			if (*oft & 0x80000000) {
 				impOrd = *oft;
 				impOrd &= 0xffff;
-				funcAddr = (DWORD)GetProcAddress(hModule, (LPCSTR)impOrd);
+				funcAddr = (DWORD)
+					GetProcAddress(hModule, (LPCSTR)impOrd);
 				if (!funcAddr) {
-					fprintf(stderr, "Couldn't find the function %d: %d\n", impOrd, GetLastError());
+					fprintf(stderr,
+							"Couldn't find the function %d: %d\n",
+							impOrd, GetLastError());
 					return FALSE;
 				}
 				*ft = funcAddr;
-			}
-			else {
-				impName = (IMAGE_IMPORT_BY_NAME *)((DWORD)loadBase + *oft);
-				funcAddr = (DWORD)GetProcAddress(hModule, impName->Name);
+			} else {
+				impName = (IMAGE_IMPORT_BY_NAME *)
+					((DWORD)loadBase + *oft);
+				funcAddr = (DWORD)
+					GetProcAddress(hModule, impName->Name);
 				if (!funcAddr) {
-					fprintf(stderr, "Couldn't find the function %s: %d\n", impName->Name, GetLastError());
+					fprintf(stderr,
+							"Couldn't find the function %s: %d\n",
+							impName->Name,
+							GetLastError());
 					return FALSE;
 				}
 				*ft = funcAddr;
