6ab50c3ad7f124ecd45a9a6ff91ff686e243b81d
George-Cristian Muraru <murarugeorgec@gmail.com>
2019-05-06 01:36:45 +0300

    HW4 remove compile warning

--- 4-scheduler/checker-win/_test/run_test.h
+++ 4-scheduler/checker-win/_test/run_test.h
@@ -10,6 +10,7 @@
 /* functions exported by the framework */
 extern void init_test(void);
 extern void cleanup_test(void);
+extern void basic_test(int test_passed);
 
 /* prototype executed for each test */
 typedef void (run_test_f)(void);
