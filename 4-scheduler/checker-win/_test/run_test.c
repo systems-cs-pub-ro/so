f3e81cb89035fe827cb1c108002cb5885abadfca
George Muraru <murarugeorgec@gmail.com>
2019-05-09 00:08:59 +0300

    HW4 possible buffer overflow

--- 4-scheduler/checker-win/_test/run_test.c
+++ 4-scheduler/checker-win/_test/run_test.c
@@ -111,10 +111,9 @@ int main(int argc, char **argv)
 	}
 
 	/* randomize time quantums */
-    srand((unsigned long)time(NULL));
+	srand((unsigned long)time(NULL));
 
  	test_fun_array[test_index].function();
-
 
 	if (fopen_s(&file, FILE_NAME, "wt") == 0) {
 		fprintf(file, "%d", 1);
