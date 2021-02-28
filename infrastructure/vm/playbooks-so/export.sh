6232f80c8d30d632e3cc1ada46000639fe35b834
Mihai-Drosi Câju <cajum.bugs@yandex.com>
2021-02-28 21:57:41 +0200

    infrastructure: Upgrade Ubuntu base image to 20.04

--- infrastructure/vm/playbooks-so/export.sh
+++ infrastructure/vm/playbooks-so/export.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vboxmanage export 'vagrantfile-so_default_1550157433689_43974' --ovf10 -o so-ubuntu-18-04.ova
+vboxmanage export 'vagrantfile-so_default_1550157433689_43974' --ovf10 -o so-ubuntu-20-04.ova
--- infrastructure/vm/vagrantfile-so/publish.sh
+++ infrastructure/vm/vagrantfile-so/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vagrant cloud publish edi33416/so-ubuntu-18.04 1.1.0 virtualbox so-ubuntu-18-04.box -d "Ubuntu 18.04 based on bento/ubuntu-18.04 and provisioned with Ansible" --version-description "Latest stable version" --release --short-description "SO VM"
+vagrant cloud publish edi33416/so-ubuntu-20.04 1.3.0 virtualbox so-ubuntu-20-04.box -d "Ubuntu 20.04 based on bento/ubuntu-20.04 and provisioned with Ansible" --version-description "Latest stable version" --release --short-description "SO VM"
