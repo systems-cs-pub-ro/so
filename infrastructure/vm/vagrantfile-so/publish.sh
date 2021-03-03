f8e872df7a0fe5086f71666932c441002fe16873
Mihai-Drosi Câju <cajum.bugs@yandex.com>
2021-03-03 21:26:36 +0200

    infrastructure: rebrand to systems-cs-pub-ro/so

--- infrastructure/vm/vagrantfile-so/publish.sh
+++ infrastructure/vm/vagrantfile-so/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vagrant cloud publish edi33416/so-ubuntu-20.04 1.3.0 virtualbox so-ubuntu-20-04.box -d "Ubuntu 20.04 based on bento/ubuntu-20.04 and provisioned with Ansible" --version-description "Latest stable version" --release --short-description "SO VM"
+vagrant cloud publish systems-cs-pub-ro/so 2.0.0 virtualbox so-ubuntu-20-04.box -d "Ubuntu 20.04 based on bento/ubuntu-20.04 and provisioned with Ansible" --version-description "Latest stable version" --release --short-description "SO VM"
