8c0f5fbe489cabc0ba6a07d090123b5848d29df2
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2022-02-22 01:12:01 +0200

    Add sol for lab11

--- labs/lab11/sol/lin/2-epollpipe/epoll.c
+++ labs/lab11/sol/lin/2-epollpipe/epoll.c
@@ -6,7 +6,6 @@
  *
  * Epoll usage
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -28,10 +27,9 @@
 #include "utils.h"
 
 static int pipes[CLIENT_COUNT][2];
-int child_pid[CLIENT_COUNT];
+int chld_pid[CLIENT_COUNT];
 
-/* TODO - Uncomment this for task 3 */
-/* #define USE_EVENTFD */
+#define USE_EVENTFD
 int event_fd;
 
 static void set_event(int index, uint64_t *event)
@@ -46,40 +44,79 @@ static int get_index(uint64_t event)
 
 static int server(void)
 {
-	int efd;				/* epoll handle */
-	struct epoll_event ev;	/* epoll event structure */
+	/* epoll handle */
+	int efd;
+
+	/* epoll event structure */
+	struct epoll_event ev;
+
 	int i;
 	int recv_msgs;
 	int recv_count;
 	char msg[MSG_SIZE];
-	int status;
-	int rc;
-	int index;
+	int status, rc, index;
 	uint64_t event;
 
-	printf("[SERVER] Started\n");
+	printf("server: started\n");
 
 	/* TODO - Init epoll and remember to close write pipe heads */
-	for (i = 0; i < CLIENT_COUNT; i++) {
+	efd = epoll_create(CLIENT_COUNT);
+	DIE(efd < 0, "epoll_create");
 
+	for (i = 0; i < CLIENT_COUNT; i++) {
+		close(pipes[i][PIPE_WRITE]);
+
+		ev.data.fd = pipes[i][PIPE_READ];
+		ev.events = EPOLLIN;
+		rc = epoll_ctl(efd, EPOLL_CTL_ADD, pipes[i][PIPE_READ], &ev);
+		DIE(rc < 0, "epoll_ctl");
 	}
+
+#ifdef USE_EVENTFD
+	ev.data.fd = event_fd;
+	ev.events = EPOLLIN;
+	rc = epoll_ctl(efd, EPOLL_CTL_ADD, event_fd, &ev);
+	DIE(rc < 0, "epoll_ctl");
+#endif
 
 	/* Number of received messages */
 	recv_msgs = 0;
 
 	while (recv_msgs < CLIENT_COUNT) {
-		/* TODO - use epoll to wait to read from pipes */
+		/* TODO - Use epoll to wait to read from pipes */
+		struct epoll_event ret_ev;
 
+		rc = epoll_wait(efd, &ret_ev, 1, -1);
+		DIE(rc < 0, "epoll_wait");
+
+		if (ret_ev.data.fd == event_fd) {
+			read(ret_ev.data.fd, &event, sizeof(uint64_t));
+
+			/* Not close pipe head */
+			index = get_index(event);
+			close(pipes[index][PIPE_READ]);
+
+			continue;
+		}
+
+		if ((ret_ev.events & EPOLLIN) != 0) {
+			recv_msgs++;
+			recv_count = read(ret_ev.data.fd, msg, MSG_SIZE);
+			DIE(recv_count < 0, "read");
+
+			msg[recv_count] = '\0';
+			printf("received:%s\n", msg);
+		}
 	}
 
-	printf("[SERVER] Going to wait for clients to end\n");
+	printf("server: going to wait for clients to end\n");
 
 	for (i = 0; i < CLIENT_COUNT; ++i) {
 		rc = waitpid(ANY, &status, 0);
 		DIE(rc < 0, "waitpid");
 	}
 
-	printf("[SERVER] Exiting\n");
+	printf("server: exiting\n");
 	return 0;
 }
 
@@ -87,21 +124,20 @@ static int server(void)
 static int client(unsigned int index)
 {
 	char msg[MSG_SIZE];
-	int rand_no;
-	int rc;
+	int rand_no, rc;
 	uint64_t event = 0;
 
 	/* Close read pipe head, wait random time and send a message */
-	printf("[CLIENT] %i: Started\n", index);
+	printf("client %i: started\n", index);
 
 	rc = close(pipes[index][PIPE_READ]);
-	DIE(rc < 0, "Close failed");
+	DIE(rc < 0, "close failed");
 
 	srandom(index);
 
 	sleep(random()%10);
 
-	printf("[CLIENT] %i: Writing message\n", index);
+	printf("client %i: writing message\n", index);
 
 	memset(msg, 0, MSG_SIZE);
 	rand_no = (char)(random()%30);
@@ -109,18 +145,20 @@ static int client(unsigned int index)
 	rc = write(pipes[index][PIPE_WRITE], msg, MSG_SIZE);
 	DIE(rc < 0, "write");
 
-#ifdef USE_EVENTFD
-	/* TODO 2 - Init event (see set_event()) and use it to signal the
-	 * server of our termination
-	 */
 
-	printf("[CLIENT] %d Sending MAGIC exit = 0x%lx\n", index,
-			(unsigned long)event);
+#ifdef USE_EVENTFD
+	set_event(index, &event);
+	printf(" client %d sending MAGIC exit = 0x%lx\n",
+		index, (unsigned long)event);
+
+	rc = write(event_fd, &event, sizeof(event));
+	DIE(rc < 0, "write");
+
 #endif
 
-	printf("[CLIENT] %i: Exiting\n", index);
+	printf("client %i: exiting\n", index);
 	rc = close(pipes[index][PIPE_WRITE]);
-	DIE(rc < 0, "Close");
+	DIE(rc < 0, "close");
 
 	return 0;
 }
@@ -132,6 +170,7 @@ int main(void)
 	int rc;
 	pid_t pid;
 
+
 	/* Init pipes */
 	for (i = 0 ; i < CLIENT_COUNT; i++) {
 		rc = pipe(pipes[i]);
@@ -139,8 +178,8 @@ int main(void)
 	}
 
 #ifdef USE_EVENTFD
-	/* TODO 2 - create eventfd  - (task3) */
-
+	event_fd = eventfd(0, 0);
+	DIE(event_fd < 0, "eventfd");
 #endif
 
 	/* Crate clients as child processes */
@@ -153,7 +192,7 @@ int main(void)
 			client(i);
 			return EXIT_SUCCESS;
 		}
-		child_pid[i] = pid;
+		chld_pid[i] = pid;
 	}
 
 	server();
