b7eb024e61086cbcbfefe1d65e9c02de91d6c8e1
Grigore Lucian <lucigrigo@yahoo.com>
2022-02-26 17:55:26 +0200

    fixed coding style for macros

--- labs/lab06/skel/lin/utils/utils.h
+++ labs/lab06/skel/lin/utils/utils.h
@@ -11,16 +11,14 @@
 #include <stdlib.h>
 
 /* Useful macro for handling error codes */
-#define DIE(assertion, call_description)					\
-	do									\
-	{									\
-		if (assertion)							\
-		{								\
-			fprintf(stderr, "(%s, %s, %d): ",			\
-					__FILE__, __FUNCTION__, __LINE__);	\
-			PrintLastError(call_description);			\
-			exit(EXIT_FAILURE);					\
-		}								\
+#define DIE(assertion, call_description)				\
+	do {								\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",			\
+					__FILE__, __LINE__);		\
+			perror(call_description);			\
+			exit(EXIT_FAILURE);				\
+		}							\
 	} while (0)
 
 #endif
