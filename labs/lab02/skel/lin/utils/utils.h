974c5d78ac0931180593a7c626e0c92751459a80
Vlad Corneci <vladcorneci@192-168-0-139.rdsnet.ro>
2022-03-10 11:14:39 +0200

    Replace PrintLastError with perror function

--- labs/lab02/skel/lin/utils/utils.h
+++ labs/lab02/skel/lin/utils/utils.h
@@ -1,5 +1,5 @@
 /**
- * SO, 2017
+ * SO
  * Lab #2, Operatii I/O simple
  *
  * Useful structures/macros
@@ -16,7 +16,7 @@
 		if (assertion) {						\
 			fprintf(stderr, "(%s, %s, %d): ",			\
 					__FILE__, __FUNCTION__, __LINE__);	\
-			PrintLastError(call_description);			\
+			perror(call_description);			\
 			exit(EXIT_FAILURE);					\
 		}								\
 	} while (0)
