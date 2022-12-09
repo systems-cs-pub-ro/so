3ba2e7995c14f977bee50c703ab3e3b11f6724ae
Elena Mihailescu <elenamihailescu22@gmail.com>
2022-12-09 23:14:32 +0200

    [2022/Hackathon] Add skel for 2022-2023 sem1 project
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

---
+++ hackathon/lambda-loader/skel/server.c
@@ -0,0 +1,86 @@
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "ipc.h"
+#include "server.h"
+
+#ifndef OUTPUTFILE_TEMPLATE
+#define OUTPUTFILE_TEMPLATE "../checker/output/out-XXXXXX"
+#endif
+
+static int lib_prehooks(struct lib *lib)
+{
+	return 0;
+}
+
+static int lib_load(struct lib *lib)
+{
+	return 0;
+}
+
+static int lib_execute(struct lib *lib)
+{
+	return 0;
+}
+
+static int lib_close(struct lib *lib)
+{
+	return 0;
+}
+
+static int lib_posthooks(struct lib *lib)
+{
+	return 0;
+}
+
+static int lib_run(struct lib *lib)
+{
+	int err;
+
+	err = lib_prehooks(lib);
+	if (err)
+		return err;
+
+	err = lib_load(lib);
+	if (err)
+		return err;
+
+	err = lib_execute(lib);
+	if (err)
+		return err;
+
+	err = lib_close(lib);
+	if (err)
+		return err;
+
+	return lib_posthooks(lib);
+}
+
+static int parse_command(const char *buf, char *name, char *func, char *params)
+{
+	return sscanf(buf, "%s %s %s", name, func, params);
+}
+
+int main(void)
+{
+	int ret;
+	struct lib lib;
+
+	/* TODO - Implement server connection */
+
+	while(1) {
+
+		/* TODO - get message from client */
+		/* TODO - parse message with parse_command and populate lib */
+		/* TODO - handle request from client */
+		ret = lib_run(&lib);
+	}
+
+	return 0;
+}
